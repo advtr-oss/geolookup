b5b5fd42e62ae179d7840518eb26bfe7272fea62
harrytwright <haroldtomwright@gmail.com>
2021-08-31 16:43:15 +0100

    plugins: Add file watching & cleanup
    Now we can watch for changes in the mock folder and update, also changed to use log and not proc-log
    
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- plugins/mock/scripts/npm-install.sh
+++ plugins/mock/scripts/npm-install.sh
@@ -6,5 +6,5 @@ npm install --save-dev espree recast ast-types @harrytwright/ast-types-wrapper
 echo "npm uninstall --save-dev @elastic/elasticsearch-mock"
 npm uninstall --save-dev @elastic/elasticsearch-mock
 
-echo "npm install --save glob @elastic/elasticsearch-mock"
-npm install --save glob @elastic/elasticsearch-mock
+echo "npm install --save glob @elastic/elasticsearch-mock chokidar"
+npm install --save glob @elastic/elasticsearch-mock chokidar
--- plugins/mock/src/mock.js
+++ plugins/mock/src/mock.js
@@ -7,44 +7,47 @@ const fs = require('fs')
 const path = require('path')
 
 const glob = require('glob')
+const chokidar = require('chokidar')
+
+const log = require('./log')
 
 module.exports = (mock, config) => {
-  process.emit('log', 'info', 'mock:loader', { cwd: config.get('volume') }, `Volume path set at ${config.get('volume')}`)
+  log.info('mock', { cwd: config.get('volume') }, `Volume path set at ${config.get('volume')}`)
 
-  const files = glob.sync('*.es.?(nd)json', {
-    cwd: config.get('volume')
-  }).map((file) => path.join(path.relative(process.cwd(), config.get('volume')), file))
+  let processedData = []
 
-  process.emit('log', 'info', 'mock:loader', { files: files }, `Found ${files.length} mocks to load`)
+  // Create a rough scoring, called as a function so it creates data each time
+  const scoring = memorise(() => {
+    return processedData.map((el) => ({
+      score: createFuzzyScorer(el._source.containers.entity.value),
+      id: el._id
+    }))
+  }, processedData)
 
-  const loadData = (filePath) => {
-    process.emit('log', 'info', 'mock:loader', { file: filePath, ext: path.extname(filePath) }, `Loading mock from ${filePath}`)
+  // Load the files from the mock folder
+  const load = () => {
+    log.notice('mock:loader', { cwd: config.get('volume') }, `Loading files @ ${config.get('volume')}`)
 
-    const buffer = fs.readFileSync(filePath);
-    let data;
-    if (path.extname(filePath) === '.ndjson') {
-      // ndjson
-      data = buffer.toString().trim().split('\n').map(JSON.parse)
-    } else {
-      data = JSON.parse(buffer.toString())
+    const files = glob.sync('*.es.?(nd)json', {
+      cwd: config.get('volume')
+    }).map((file) => path.join(path.relative(process.cwd(), config.get('volume')), file))
+
+    log.info('mock:loader', { files: files }, `Found ${files.length} mocks to load`)
+
+    let rawData = []
+    for (const file of files) {
+      rawData = [...rawData, ...loadData(file)]
     }
-    return data
+
+    processedData = rawData
   }
 
-  let mockableData = []
-  for (const file of files) {
-    mockableData = [...mockableData, ...loadData(file)]
-  }
+  // Call load
+  log.notice('mock:bootstrap', 'Initial call to load')
+  load()
 
-  const scoring = mockableData.map((el) => ({
-    score: createFuzzyScorer(el._source.containers.entity.value),
-    id: el._id
-  }))
-
-  /**
-   * Add a health check
-   * */
-  process.emit('log', 'info', 'mock:add', { path: '/_cluster/health', method: ['GET'] }, `Added mock for '/_cluster/health'`)
+  // Add the healthcheck
+  log.info('mock:add', { path: '/_cluster/health', method: ['GET'] }, `Added mock for '/_cluster/health'`)
   mock.add({
     method: ['GET'],
     path: '/_cluster/health'
@@ -53,16 +56,17 @@ module.exports = (mock, config) => {
     status: 'green'
   }))
 
-  process.emit('log', 'info', 'mock:add', { path: '/geospatial/_search', method: ['GET', 'POST'] }, `Added mock for '/geospatial/_search'`)
+  // Add the fake search
+  log.info('mock:add', { path: `/${config.get('elastic-index')}/_search`, method: ['GET', 'POST'] }, `Added mock for '/${config.get('elastic-index')}/_search'`)
   mock.add({
     method: ['GET', 'POST'],
-    path: '/geospatial/_search'
+    path: `/${config.get('elastic-index')}/_search`
   }, (params) => {
     // This should work, as long as the query stays the same
     const input = params.body.query.bool.must[0].match['containers.entity.value'].query
-    const results = scoring.filter((el) => el.score(input) >= 1).map((el) => el.id)
+    const results = scoring().filter((el) => el.score(input) >= 1).map((el) => el.id)
 
-    let hits = mockableData.filter((el) => results.includes(el._id))
+    let hits = processedData.filter((el) => results.includes(el._id))
 
     if (params.body.query.bool.must[1]) {
       hits = hits.filter((hit) => params.body.query.bool.must[1].terms.country_code.includes(hit._source.country_code))
@@ -71,7 +75,54 @@ module.exports = (mock, config) => {
     return { hits: { hits } }
   })
 
+  log.notice('mock:bootstrap', { }, 'Setting chokidar to watch for mock changes')
+  chokidar.watch('*.es.?(nd)json', { ignored: /^\./, persistent: true, cwd: config.get('volume'), ignoreInitial: true })
+    .on('add', (path) => {
+      log.info('mock:watcher', { path }, `File added to mock folder @ ${path}`)
+      load()
+    }).on('change', (path) => {
+    log.info('mock:watcher', { path }, `File updated in mock folder @ ${path}`)
+    load()
+  })
+
   return mock
+}
+
+function Memorised(count, result, deps) {
+  this.count = count
+  this.result = result
+  this.deps = JSON.stringify(deps)
+}
+
+/**
+ * Function to be save time on when calling score and update only when their is a change to dependencies
+ *
+ * Very rough work at a memorisation function
+ * */
+function memorise(fn, deps) {
+  let count = 0
+  const dependency = {}
+
+  return (...args) => {
+    if (!dependency[fn] || dependency[fn].deps !== JSON.stringify(deps)) {
+      dependency[fn] = new Memorised(count, fn(...args), deps)
+    }
+    return dependency[fn].result
+  }
+}
+
+function loadData(filePath) {
+  log.info('mock:loader', { file: filePath, ext: path.extname(filePath) }, `Loading mock from ${filePath}`)
+
+  const buffer = fs.readFileSync(filePath);
+  let data;
+  if (path.extname(filePath) === '.ndjson') {
+    // ndjson
+    data = buffer.toString().trim().split('\n').map(JSON.parse)
+  } else {
+    data = JSON.parse(buffer.toString())
+  }
+  return data
 }
 
 /**
